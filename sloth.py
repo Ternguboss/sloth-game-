f6abd0e56a7cb8563a632e0baea34322c6a08cb8
Terngu Boss <terngufavour35@gmail.com>
2026-06-05 15:10:33 -0700

    worked on the display for the sloth machine and started checking function

--- sloth.py
+++ sloth.py
@@ -1,6 +1,48 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count ={
+    "A": 2,
+    "B": 4,
+    "C": 5,
+    "D": 8,
+}
+
+def sloth_spin( rows , cols ,symbols):
+    all_symbols = []
+    for symbol , symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns = []
+    for  _  in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for  _  in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_sloth_machine(columns):
+    for row in range (len(columns[0])):
+        for i ,column in enumerate(columns):
+            if i != len(columns) -1 :
+                print(column[row], end="|" )
+            else:
+                print(columns[row], end="")
+
+        print()
 
 def deposit():
     while True:
@@ -58,6 +100,8 @@ def main():
             break
 
     print(f"you are beting ${bet} on ${lines} lines. total bet is equal to ${total_bet}  ")
-    print(balance , lines)
 
+    slots =  sloth_spin (ROWS ,COLS , symbol_count )
+    print_sloth_machine(slots)
+    
 main()      
